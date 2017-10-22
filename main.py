ecaae1ebcfa0a5cdc73dead7823b5f5a284ad372
Thomas Estep <estep.132@osu.edu>
2017-10-21 22:39:00 -0400

    Test stuff

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-import Salary
+import Salary.salary as salary
 
 
 def main():
-    return None
+    soft_eng = salary.H1BData()
+    soft_eng.base_url = 'http://visadoor.com/h1b/index?company=&job=software+engineer&state=&year=2016&submit=Search'
+    salaries = soft_eng.pull_data()
+    for k, v in salaries.items():
+        print('Company:', k)
+        print('Title:', v[0])
+        print('Salary:', v[1], '\n')
 
 
 if __name__ == '__main__':
